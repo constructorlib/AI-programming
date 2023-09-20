52b4c9c25431852f5f7980e75c8a9fd8e55c9fe9
constructorlib <constructorlib@gmail.com>
2023-09-20 11:52:45 +0900

    Assignment 2

--- aip2_assign2_20202037.py
+++ aip2_assign2_20202037.py
@@ -1,1 +1,8 @@
-# DD tomorrow (?)
+import pandas as pd
+
+
+# Define column names
+column_names = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
+
+# Load the data into a DataFrame
+df = pd.read_csv('actual_file_path_here.csv', header=None, delimiter=r"\s+", names=column_names)
